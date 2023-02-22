1c13babd16505702442be55ca1d814c5e9ab655c
mdasifmunshe <asif35-2872@diu.edu.bd>
2023-02-22 11:04:55 +0600

    NodeJS Api design v4

--- nodeJS/node-js-v2/server/server.mjs
+++ nodeJS/node-js-v2/server/server.mjs
@@ -1,50 +1,79 @@
-import express from "express";
-import morgan from "morgan";
-import bp from "body-parser";
+import express from 'express';
+import morgan from 'morgan';
+import bp from 'body-parser';
 
 const app = express();
 
-const HOST = "localhost";
+const HOST = 'localhost';
 const PORT = 8080;
 
 app.use(bp.urlencoded({ extended: true }));
 app.use(bp.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 const db = {
-  todos: [],
+	todos: [],
 };
 
-// Create
-app.post("/postTodo", (req, res) => {
-  const newTodo = {
-    complete: false,
-    id: Date.now(),
-    text: req.body.text,
-  };
-  db.todos.push(newTodo);
+// Default
+app.get('/', (req, res) => {
+	res.json({ message: 'Hello from Server' });
+});
 
-  res.json({ data: newTodo });
+// Create
+app.post('/postTodo', (req, res) => {
+	const newTodo = {
+		complete: false,
+		id: Date.now(),
+		text: req.body.text,
+	};
+	db.todos.push(newTodo);
+
+	res.json({ data: newTodo });
 });
 
 // READ
-app.get("/getTodos", (req, res) => {
-  res.json({ data: db.todos });
+app.get('/getTodos', (req, res) => {
+	res.json({ data: db.todos });
 });
 
-app.get("/getTodo/:id", (req, res) => {
-  const todo = db.todos.find((todo) => {
-    return todo.id === +req.params.id;
-  });
+app.get('/getTodo/:id', (req, res) => {
+	try {
+		const todo = db.todos.find((todo) => {
+			return todo.id === +req.params.id;
+		});
 
-  res.json({ data: todo });
+		res.json({ data: todo });
+	} catch (error) {
+		res.status(400).json({ message: 'Invalid id' });
+	}
 });
 
 // Update
+app.put('/updateTodo/:id', (req, res) => {
+	try {
+		const id = req.params.id;
+		const text = req.body.text;
+
+		const todo = db.todos.find((todo) => todo.id == id);
+		todo.text = text;
+
+		res.json({ data: todo });
+	} catch (error) {
+		res.status(400).json({ message: 'Invalid id' });
+	}
+});
 
 // DELETE
-app.delete("/deleteTodo/:id", (req, res) => {});
+app.delete('/deleteTodo/:id', (req, res) => {
+	const id = req.params.id;
+
+	const todoIndex = db.todos.findIndex((todo) => todo.id == id);
+	db.todos.splice(todoIndex, 1);
+
+	res.json({ message: `ID: ${id} has been deleted` });
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://${HOST}:${PORT}`);
+	console.log(`Server is running on http://${HOST}:${PORT}`);
 });
